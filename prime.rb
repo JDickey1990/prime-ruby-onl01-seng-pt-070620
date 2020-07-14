231a01844f54bbb39fc9a56a57d2e9a0d8607da9
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-07-14 19:16:22 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,12 @@
 
 def prime?(number)
- if number < 0
-  return false
- elsif (2..(number - 1)).each do |n|
+ if 
+   number < 0
+  false
+ elsif 
+ (2..(number - 1)).each do |n|
   number % n ==0 
-   return false
+   false
   end
      true
   end
